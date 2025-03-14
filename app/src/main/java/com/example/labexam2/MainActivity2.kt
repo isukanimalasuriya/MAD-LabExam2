e10d06c89f62248270312d50fb0505f43d0c97a3
isukanimalasuriya <anujitha1234@gmail.com>
2025-03-14 20:36:35 +0530

    Bugs fixed

--- app/src/main/java/com/example/labexam2/MainActivity2.kt
+++ app/src/main/java/com/example/labexam2/MainActivity2.kt
@@ -5,16 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bottom_home -> {
+                    // Load Home Fragment
+                    true
+                }
+                R.id.bottom_navigation -> {
+                    // Load Dashboard Fragment
+                    true
+                }
+                R.id.navigation_notifications -> {
+                    // Load Notifications Fragment
+                    true
+                }
+                else -> false
+            }
         }
+
     }
 }
